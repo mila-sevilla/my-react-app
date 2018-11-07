e6ca47396fe0ebaa142dc02af9947618047306a3
msevilla <m.sevillazhou@gmail.com>
2018-11-07 12:56:53 +0100

    Basic CSS for the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,13 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
